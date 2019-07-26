6271b0d300c41122d70fb461ca7f5b482df24294
AlejandraJaramillo <alejandracjaramillo@hotmail.com>
2019-07-26 10:23:33 +0200

    dependencies updated

--- server.js
+++ server.js
@@ -1,27 +1,24 @@
-var express = require('express'),
+var express = require("express"),
     app = express(),
     port = process.env.PORT || 3000,
-    mongoose = require('mongoose'),
-    Task = require('./API/models/apiModel'), //created model loading here
-    bodyParser = require('body-parser');
+    mongoose = require("mongoose"),
+    Task = require("./API/models/apiModel"), //created model loading here
+    bodyParser = require("body-parser");
 
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb', { useNewUrlParser: true });
-
-
+mongoose.connect("mongodb://localhost/Tododb", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'});
+    res.status(404).send({ url: req.originalUrl + " not found" });
 });
 
-var routes = require('./api/routes/apiRoutes'); //importing route
+var routes = require("./api/routes/apiRoutes"); //importing route
 routes(app); //register the route
-
 
 app.listen(port);
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log("todo list RESTful API server started on: " + port);
